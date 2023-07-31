a9c1a15b0034f6fa843a561680735865c94624d6
Gourav <Gouravkumar.1273@gmail.com>
2023-07-31 09:55:17 +0530

    Making apps error proof

--- src/App.js
+++ src/App.js
@@ -6,22 +6,36 @@ import './App.css';
 function App() {
   const[movies,setMovies]=useState('');
   const[loading,setLoading]=useState(false);
+  const[error,setError]=useState(null);
 
 
  async function fetchMoviesHandler(){
   setLoading(true);
-  const response =await fetch('https://swapi.dev/api/films/') 
-  const data=await response.json();
-  const transformedMovies=data.results.map(movieData=>{
-        return{
-          id:movieData.episode_id,
-          title:movieData.title,
-          openingText:movieData.opening_crawl,
-          releaseDate:movieData.release_date,
-        };
-      })
-      setMovies(transformedMovies);
-      setLoading(false);
+  setError(null);
+  try{
+    const response =await fetch('https://swapi.dev/api/films/') 
+     
+       if(!response.ok)
+      throw new Error('Retrying')
+    
+
+    const data=await response.json();
+    
+    const transformedMovies=data.results.map(movieData=>{
+          return{
+            id:movieData.episode_id,
+            title:movieData.title,
+            openingText:movieData.opening_crawl,
+            releaseDate:movieData.release_date,
+          };
+        })
+        setMovies(transformedMovies);
+    }
+    catch(error){
+      setError(error.message);
+    }
+    setLoading(false);
+
  };
   return (
     <React.Fragment>
@@ -30,11 +44,13 @@ function App() {
 
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
+      <section> 
         {!loading&& movies.length>0&&<MoviesList movies={movies} />}
-        {loading&&movies.length===0&& <p>Loading</p>}
-        
+        {!loading&&movies.length===0&&!error&& <p>No Movie FOund</p>}
         {loading&& <p>Loading</p>}
+        
+        {!loading && error && <p>Error: {error}</p>}
+        {!loading&&error&& <button onClick={()=>setError(!error)}> Cancel Retrying </button>}
       </section>
     </React.Fragment>
   );
